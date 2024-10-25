cead0df40b3dc20498b69f9e1025f80a90f27a65
Etec Prof. Horácio Augusto da Silveira <enzo_negrao_freitas@outlook.com>
2024-10-24 22:30:00 -0300

    Criação do projeto e dos métodos CRUD

---
+++ src/main/java/br/com/etechoracio/study/service/DisciplinaService.java
@@ -0,0 +1,47 @@
+package br.com.etechoracio.study.service;
+
+import br.com.etechoracio.study.entity.Disciplina;
+import br.com.etechoracio.study.repository.DisciplinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DisciplinaService {
+    @Autowired
+    private DisciplinaRepository repository;
+
+    public List<Disciplina> listar(){
+        return repository.findAll();
+    }
+
+    public Disciplina buscarPorId(Long id) {
+        var existe = repository.findById(id);
+        if(existe.isPresent())
+            return existe.get();
+        return null;
+    }
+
+    //Cadastrar disciplina
+    public Disciplina cadastrar(Disciplina disciplina){
+         return repository.save(disciplina);
+    }
+
+    //Alterar
+    public Disciplina alterar(Disciplina disciplina){
+        var existe = buscarPorId(disciplina.getId());
+        if (existe != null)
+            return repository.save(disciplina);
+        else {
+            System.out.println("Disciplina não encontrada");
+            return null;
+        }
+    }
+
+    public void excluir (Long id){
+        var existe = buscarPorId(id);
+        if (existe != null)
+            repository.deleteById(id);
+    }
+}
